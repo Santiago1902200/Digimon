4fe401875510949b97ada1bbadc0b00baa892770
Santiago1902200 <santiagocd00@gmail.com>
2025-04-10 19:28:41 +0000

    digimon v1

--- js/api.js
+++ js/api.js
@@ -1,17 +1,10 @@
-const TOKEN = "BQBRvpPv8gG8GtZYXAXfAx8xsq6ZWldGCIZ1WNk-79VRuz-xt3sr3ZoqRLDjZCF8IdRTbDbzxVF7aPH4l1wrlkvfK7jqo8cERU1UTSUEFelwt56ccO7tdqTgb1_q2TsmtEDJzGDl7vQjjZGLQtojO36biInc78-3UnudaKri8_xdXtk1vUIuzA6YfBZ1ASuBnoD5hjig-ul_6JPTyqYmDqarXULsfdmrWEN7z6WkgwsOkIn4iAeprz_InYIUxmZh0yUqdyC_aqWK2cPy-vaYOQX68AVv2PaI0zftGqcJJE1hEhbJc0D99UFi"; // reemplaza esto con tu token temporal de Spotify
-
 async function conexionLista() {
-    const res = await fetch('https://api.spotify.com/v1/search?q=rock&type=artist&limit=20', {
-        headers: {
-            Authorization: `Bearer ${TOKEN}`
-        }
-    });
-
+    const res = await fetch('https://digimon-api.vercel.app/api/digimon');
     const data = await res.json();
-    return data.artists.items;
+    return data.slice(0, 20); // mostramos solo los primeros 20
 }
 
 async function general() {
-    const artistas = await conexionLista();
-    mostrarLista(artistas);
+    const digimones = await conexionLista();
+    mostrarLista(digimones);
 }
--- js/lista.js
+++ js/lista.js
@@ -1,17 +1,16 @@
-function mostrarLista(artistas) {
+function mostrarLista(digimones) {
     const app = document.getElementById("app");
-    app.innerHTML = "Lista de Artistas";
+    app.innerHTML = "Lista de Digimon";
 
     let contenido = "";
-    for (let i = 0; i < artistas.length; i++) {
-        const artista = artistas[i];
-        const imagen = artista.images[0] ? artista.images[0].url : "default.jpg"; // por si no tiene imagen
-
+    for (let i = 0; i < digimones.length; i++) {
+        const digi = digimones[i];
         contenido += `
-            <div class="c-lista-artista" onclick="mostrarDetalle('${artista.id}')">
-                <img src="${imagen}" width="auto" height="60" loading="lazy" alt="${artista.name}">
-                <p>${artista.name}</p>
+            <div class="c-lista-digimon" onclick="mostrarDetalle('${digi.name}')">
+                <img src="${digi.img}" width="auto" height="60" loading="lazy" alt="${digi.name}">
+                <p>${digi.name}</p>
             </div>`;
     }
     app.innerHTML = contenido;
 }
+
